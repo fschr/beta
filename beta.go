e575d202bafbb53412ab8890a145619a2669d18f
Thomas Fischer <thomas.fischer@coreos.com>
2016-09-21 09:31:57 -0700

    Add appropriate copyright headers and attributions to beta.go.

--- beta.go
+++ beta.go
@@ -1,3 +1,24 @@
+/* Copyright (C) 1996, 1997, 1998, 1999, 2000 Gerard Jungman
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+/* Author:  G. Jungman */
+/* Ported by: Thomas Fischer | github.com/gragas */
+
+/* beta is a port of some functions found in GSL's specfunc/beta_inc.c */
 package beta
 
 import "math"
